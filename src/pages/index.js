d17782bc3401b42ec50327d22b901dbcdfb042bb
Aashish Nepal <nepal.aashish00@gmail.com>
2025-06-10 23:06:15 +0545

    Fix global CSS import issue

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,6 @@ import About from '../components/About';
 import Experience from '../components/Experience';
 import Projects from '../components/Projects';
 import Contact from '../components/Contact';
-import "../styles/globals.css";
 
 export default function Home() {
   return (
