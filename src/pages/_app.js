c67ec5bc0eb9d06873e7d683cf49fae32bd1840b
Aashish Nepal <nepal.aashish00@gmail.com>
2025-06-16 01:52:06 +0545

    Some Fixes

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,6 +3,7 @@ import { useEffect } from 'react';
 import "../styles/globals.css";
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
   // Initialize dark mode from localStorage
@@ -18,6 +19,10 @@ function MyApp({ Component, pageProps }) {
   }, []);
 
   return (
+    <>
+    <Head>
+      <title>Aashish Nepal</title>
+    </Head>
     <div className="bg-gray-50 dark:bg-gray-900 text-gray-900 dark:text-gray-100 min-h-screen transition-colors duration-300">
       <Header />
       <main className="max-w-8xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -25,7 +30,8 @@ function MyApp({ Component, pageProps }) {
       </main>
       <Footer />
     </div>
-  );
+    </>
+  ); 
 }
 
 export default MyApp;
